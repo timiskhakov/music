ef4b370dfe0478f3aa58f04b36630de40468626c
Timur Iskhakov <timiskhakov@gmail.com>
2022-04-29 16:06:43 +0300

    Extract run method

--- main.go
+++ main.go
@@ -13,12 +13,29 @@ import (
 const sampleRate = 44100
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	kse := karplusstrong.NewExtended(sampleRate)
 	g := guitar.NewGuitar(sampleRate, kse)
 	s := song.Portal(g)
 
-	f, _ := os.Create("portal.wav")
-	wav.Encode(f, s, beep.Format{SampleRate: sampleRate, NumChannels: 2, Precision: 3})
+	name := "portal.wav"
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
 
-	fmt.Println("Done")
+	fmt.Printf("Saving to %s...\n", name)
+	if err = wav.Encode(f, s, beep.Format{SampleRate: sampleRate, NumChannels: 2, Precision: 3}); err != nil {
+		return err
+	}
+
+	fmt.Println("Saved.")
+
+	return nil
 }
